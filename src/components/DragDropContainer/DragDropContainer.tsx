93093e9ab4a4662028438c796fa83c7ed232a00f
Ilya Makhin <79757684+zubastu@users.noreply.github.com>
2024-03-20 18:39:25 +0300

    перенес конмпонент кнопки в кноструктор, всерстка компонента taskItem

--- src/components/DragDropContainer/DragDropContainer.tsx
+++ src/components/DragDropContainer/DragDropContainer.tsx
@@ -1,12 +1,12 @@
 import { DndProvider } from "react-dnd";
 import { TouchBackend } from "react-dnd-touch-backend";
-import { ReactNode } from "react";
+import { FC, ReactNode } from "react";
 
 type TDragDropContainerProps = {
   children: ReactNode;
 };
 
-const DragDropContainer: React.FC<TDragDropContainerProps> = ({ children }) => {
+const DragDropContainer: FC<TDragDropContainerProps> = ({ children }) => {
   return (
     <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
       {children}
--- src/components/TaskContainer/TaskContainer.tsx
+++ src/components/TaskContainer/TaskContainer.tsx
@@ -1,11 +1,14 @@
 import styles from "./styles.module.scss";
 import ActiveTasks from "../ActiveTasks/ActiveTasks.tsx";
 import CompletedTasks from "../CompleedTasks/CompletedTasks.tsx";
+import DragDropContainer from "../DragDropContainer/DragDropContainer.tsx";
 
 const TaskContainer = () => {
   return (
     <section className={styles.container}>
-      <ActiveTasks />
+      <DragDropContainer>
+        <ActiveTasks />
+      </DragDropContainer>
       <CompletedTasks />
     </section>
   );
